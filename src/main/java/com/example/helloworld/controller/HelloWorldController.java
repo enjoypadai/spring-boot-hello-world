ad01a10f44a4cffab8b6c9d37beda0a6274e1b04
rakesh <Rakesh@applicate.in>
2024-01-31 01:41:00 +0530

    c

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -1,7 +1,12 @@
 package com.example.helloworld.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 @RestController
 public class HelloWorldController {
@@ -10,4 +15,22 @@ public class HelloWorldController {
     public String sendGreetings() {
         return "Hello, World!";
     }
+
+
+    @GetMapping(value = "/getDate/{url}")
+    public String getData(@PathVariable String url) throws Exception {
+        URL oracle = new URL("https://"+url);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(oracle.openStream()));
+        StringBuilder inputLineS = new StringBuilder();
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            System.out.println(inputLine);
+            inputLineS.append(inputLine);
+        }
+        in.close();
+        return  inputLineS.toString();
+    }
+
+
 }
